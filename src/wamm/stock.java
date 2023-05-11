be52a9d4ae78a6aff12e38d97a28ab4348835f5e
ahmedadel6634 <130157241+ahmedadel6634@users.noreply.github.com>
2023-05-12 02:37:12 +0300

    Ahmed Adel Abdelhady

---
+++ src/wamm/stock.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wamm;
+import java.util.*;
+import java.util.Collection;
+import java.util.Map;
+/**
+ *
+ * @author Lenovo
+ */
+public class stock extends inventory {
+  int Product_quantity=10;
+      HashMap <Product,Integer> product_quantity=new HashMap();
+    Iterator<Map.Entry<Product,Integer>> it= product_quantity.entrySet().iterator();
+//    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+  
+      /*  public int Alert1(){
+            int x;
+        if(product_quantity.containsValue(x)<10){
+            
+        System.out.print( "order products from inventory");
+       return this.Number_products=11;
+        }
+        else{ 
+            return 0;
+                
+ }
+        
+        }
+                public int Alert(){
+            
+        if(product_quantity.containsValue(9)){
+            
+        System.out.print( "order products from inventory");
+       return this.Number_products=11;
+        }
+        else{ 
+            return 0;
+                
+ }
+        
+        }*/
+        } 
+
